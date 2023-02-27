0cec14201348241d6e3bb5d0dd4a5358f0f67c87
NazaRiks <nazariks123@ukr.net>
2023-02-27 15:30:03 +0100

    Dodaj izračun pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -7,4 +7,12 @@ public class Gravitacija {
         System.out.println("OIS je zakon!");
 
     }
+    public static double gravitacija(double v) {
+        double C = Double.parseDouble("6.674E-11");
+        double M = Double.parseDouble("5.972E24");
+        double r = Double.parseDouble("6.371E6");
+        double a = (C*M)/((r+v)*(r+v));
+
+        return a;
+    }
 }
